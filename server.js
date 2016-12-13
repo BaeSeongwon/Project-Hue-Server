39a750fc9e5a9f18137e606b51fb5ab3900320b8
성원 <seongwon9179@gmail.com>
2016-12-13 20:37:32 +0900

    '/' 테스트 라우팅 생성

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var express = require('express');
 
 var app = express();
 
+app.get('/',function(req,res){
+    res.send('<h1>아마존 AWS 서버</h1>');
+});
+
 http.createServer(app).listen('3000',function(){
     console.log("프로젝트 휴 서버 생성 성공 포트 3000!!");
+
 });
