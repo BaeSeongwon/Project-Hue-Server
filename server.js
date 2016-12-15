b00ced5f91b5c4b509c343ca5eaa040e363647c4
Parksubin/동서대학교 <tnqls8336@gmail.com>
2016-12-16 04:49:51 +0900

    subin main server commit

--- server.js
+++ server.js
@@ -24,23 +24,109 @@ app.use(session({
     saveUninitialized: true
 }));
 
+
+app.use(express.static(__dirname));
+
+
 //TODO:Server구동
 http.createServer(app).listen('80',function(){
     console.log('서버실행했다장보리!!');
-    //TODO:DB Connection
-    pool = mysql.createPool({
-        connectionLimit: 15,
-        host: 'hue.c4yiq5vuopvi.ap-northeast-2.rds.amazonaws.com',
-        user: 'root',
-        database:'HueDB',
-        password: 'seongwon9179'
-    });
+});
+
+
+//TODO:DB Connection
+var pool = mysql.createPool({
+    connectionLimit: 15,
+    host: 'localhost',
+    user: 'root',
+    database:'web',
+    password: '1234'
 });
 
 
 
-app.use(express.static(__dirname));
+//TODO:WEMO 검색
+app.get('/search' ,function (req,res) {
 
+    console.log("왔음");
+
+    request({
+        // url: 'http://192.168.0.26:3000/searchwemo',
+        url: 'http://cutesubini.iptime.org:70/searchwemo',
+        method: 'get'
+    }, function(error, response, body){
+        if(error) {
+            console.log(error);
+        } else {
+
+           var friendlyName = body[0].friendlyName;
+            var serialNumber= body[0].serialNumber;
+            var id="subin";
+
+            res.end("끝");
+        }
+    });
+
+});
+
+//TODO:WEMO 제어(완료)
+app.post('/wemocontrol' ,function (req,res) {
+    console.log("왔음");
+
+    console.log(req.body.wemo);
+    var a = req.body.wemo;
+    request({
+        // url: 'http://192.168.0.26:3000/wemocontrol',
+        url: 'http://cutesubini.iptime.org:70/wemocontrol',
+        method: 'post',
+        json: {
+            wemo: a
+        }
+    }, function(error, response, body){
+            var instantPower =body.instantPower;
+            var ONSince = body.data.ONSince;
+            var OnFor=body.data.OnFor;
+            var TodayONTime= body.data.TodayONTime;
+            var TodayConsumed = body.data.TodayConsumed;
+
+
+        pool.getConnection(function (err,connection) {
+
+            var sql ="select Id from wemo where Id=?";
+            connection.query(sql,req.session.user_id,function (err,data) {
+
+                if(data==""){
+                    var data =[req.session.user_id,a,ONSince,OnFor,TodayONTime,TodayConsumed,instantPower];
+                    var sql2 = "insert into wemo(Id,state,OnSince,OnFor,TodayONTime,TodayConsumed,instantPower) values(?)";
+
+                    connection.query(sql2,[data],function (err, data) {
+
+                        if (err) console.error("err : " + err);
+                        console.log(data);
+                        res.send("삽입 완료!");
+                        connection.release();
+                    });
+                }
+
+                else {
+
+
+                    var sql3="update wemo set state=?,OnSince=?,TodayONTime=?,TodayConsumed=?,instantPower=? where Id=?";
+                    connection.query(sql3,[a,ONSince,OnFor,TodayONTime,TodayConsumed,instantPower,req.session.user_id],function (err,suc) {
+
+                        if (err) console.error("err : " + err);
+                        console.log(data);
+                        res.send("삽입 완료!");
+                        connection.release();
+                    })
+                }
+            });
+
+
+        });
+    });
+
+});
 
 
 /* GET home page. */
@@ -49,72 +135,229 @@ app.get('/', function(req, res, next) {
 });
 
 
-app.get('/wemoControl',function (req,res,next) {
+app.get('/huecontrolOff',function (req,res,next) {
+    console.log("hue off 들어옴");
+    request({
+        // url: 'http://192.168.0.26:3000/huecontrolOff',
+        url: 'http://cutesubini.iptime.org:70/huecontrolOff',
+        method: 'get',
+        type:'JSON'
+    }, function(error, response, suc){
 
-    // var state = req.body.state;
+        var ddd = JSON.parse(suc);
+        console.log(ddd);
+        console.log(ddd.data.color);
+        console.log(ddd.data.state);
+        var state = "false";
 
-    var a = {
-        "name":"Developer Bridge"
-    }
-    var options = {
-        // url: 'http://192.168.0.19/api/XIZOy152etSXOyNpcKuOw-xcwkkRRCoCIkaBUk-z/lights/3/state',
-        url: 'http://192.168.0.26:8000/test',
-        method:"get",
-        body:JSON.stringify(a)
 
-    };
-    function subin(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var info = JSON.parse(body);
-            console.log(info);
-        }
-    }
-    request(options,subin);
+        var color= ddd.data.color;
+        var bright= ddd.data.bright;
+        console.log("state값 확인");
+        console.log(state);
+        var color= ddd.data.color;
+        var bright= ddd.data.bright;
+        console.log("state값 확인");
+        console.log(state);
+        pool.getConnection(function (err,connection) {
 
+            var sql ="select Id from hue where Id=?";
+            connection.query(sql,req.session.user_id,function (err,data) {
+
+                if(data==""){
+                    var data =[req.session.user_id,state,color,bright];
+                    var sql2 = "insert into hue(Id,state,color,bright) values(?)";
+
+                    connection.query(sql2,[data],function (err, data) {
+
+                        if (err) console.error("err : " + err);
+                        console.log(data);
+                        res.send("삽입 완료!");
+                        connection.release();
+                    });
+                }
+
+                else {
+
+                    var sql3="update hue set state=?,color=?,bright=? where Id=?";
+                    connection.query(sql3,[state,color,bright,req.session.user_id],function (err,suc) {
+
+                        if (err) console.error("err : " + err);
+                        console.log(data);
+                        res.send("삽입 완료!");
+                        connection.release();
+                    })
+                }
+            });
+
+
+        });
+
+    });
+
+});
+
+app.get('/huecontrolOn',function (req,res,next) {
+
+    console.log("hue on 들어옴");
+
+    request({
+        // url: 'http://192.168.0.26:3000/huecontrolOn',
+        url: 'http://cutesubini.iptime.org:70/huecontrolOn',
+        method: 'get'
+    }, function(error, response, suc){
+        var ddd = JSON.parse(suc);
+        console.log(ddd);
+        console.log(ddd.data.color);
+        console.log(ddd.data.state);
+        var state = "true";
+
+
+        var color= ddd.data.color;
+        var bright= ddd.data.bright;
+        console.log("state값 확인");
+        console.log(state);
+
+        pool.getConnection(function (err,connection) {
+
+            var sql ="select Id from hue where Id=?";
+            connection.query(sql,req.session.user_id,function (err,data) {
+
+                if(data==""){
+                    var data =[req.session.user_id,state,color,bright];
+                    var sql2 = "insert into hue(Id,state,color,bright) values(?)";
+
+                    connection.query(sql2,[data],function (err, data) {
+
+                        if (err) console.error("err : " + err);
+                        console.log(data);
+                        res.send("삽입 완료!");
+                        connection.release();
+                    });
+                }
+
+                else {
+
+                    var sql3="update hue set state=?,color=?,bright=? where Id=?";
+                    connection.query(sql3,[state,color,bright,req.session.user_id],function (err,suc) {
+
+                        if (err) console.error("err : " + err);
+                        console.log(data);
+                        res.send("삽입 완료!");
+                        connection.release();
+                    })
+                }
+            });
+
+
+        });
+
+
+    });
+});
+
+
+//TODO:HUE 기기정보
+app.get('/searchhue',function (req,res,next) {
+
+    request({
+        // url: 'http://192.168.0.26:3000/searchhue',
+        url: 'http://cutesubini.iptime.org:70/searchhue',
+        method: 'get'
+    }, function(error, response, body){
+        console.log(body);
+        res.end("ㅎㅇ");
+    });
+
+});
+
+//TODO:회원가입
+app.post('/Join' ,function (req,res,next) {
+
+    console.log("왓다데이터!!!!!!!!!!!!!!");
+    
+    var id =req.body.id;
+    var password= req.body.password;
+    var cpe_key=req.body.cpe_key;
+    var ip="192.168.0.26";
+    
+    pool.getConnection(function (err,connection) {
+
+        var data =[id,password,cpe_key,ip];
+       var sql = "insert into User(Id,password,cpe,ip) values(?)";
+
+        connection.query(sql,[data],function (err, data) {
+
+            if (err) console.error("err : " + err);
+            console.log(data);
+            res.send("회원가입 완료!");
+            connection.release();
+        });
+    });
+    
 });
 
 
 //TODO:로그인
-app.get('/Login',function (req,res) {
+app.post('/Login',function (req,res) {
+    
+    console.log("로그인 들어옴");
     var user = {
-        id: 'root',
-        password: '1234'
+        id:req.body.id,
+        password:req.body.password
     };
+    // var user = {
+    //     id:"shanik21",
+    //     password:"tosorl13"
+    // };
 
     pool.getConnection(function (err,connection) {
 
-        var sql = "select Id,password from user where Id=?";
+        var sql = "select Id,password,cpe from User where Id=?";
 
         connection.query(sql,[user.id],function (err,data) {
 
             for(key in data){
 
+                console.log(data);
                 var db= {
                     key_id:data[key].Id,
-                    key_password:data[key].password
+                    key_password:data[key].password,
+                    key_cpe_key:data[key].cpe
                 }
             }
             if(data=="")
             {
+                console.log("10000");
                 res.send("아이디가 존재하지 않습니다.");
             }
 
             else if(user.password===db.key_password&&user.id===db.key_id)
             {
-
+                console.log("20000");
                 req.session.user_id = user.id;
+                req.session.user_cpe=db.key_cpe_key;
 
-                res.send("로그인 완료.");
+                console.log(req.session.user_id);
+                console.log(req.session.user_cpe);
+
+                res.cookie('id',req.session.user_id);
+                res.cookie('cpe',req.session.user_cpe);
+                res.send("로그인 완료");
 
             }
-
             else if (user.password===db.key_password||user.id===db.key_id)
             {
-
+                console.log("30000");
                 res.send("다시한번 확인 해주세요.");
-
             }
         });
     });
 });
 
+app.get('/Logout',function (req,res,next) {
+    req.session.destroy();
+    res.clearCookie('id');
+    res.clearCookie('cpe');
+    res.end("Logout");
+});
